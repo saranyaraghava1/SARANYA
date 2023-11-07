26db2bb27ca69d0b7561f5e7a586bc2bb5115c66
saranyaraghava1 <727722euit166@skcet.ac.in>
2023-11-07 16:05:06 +0530

    commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Main = () => {
   return (
     <div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiIDZZ2zobYDZVnOsSp8v5FENYIo7QKNyIdLYyL5xLAoGJVNvTmtIRkt6vW_FKDmOnt98&usqp=CAU"></img>
       <center>
         <label for="name">Email Id: &emsp;</label>
         <input id="name" type="box" placeholder="Email ID"></input>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div>
       <hr></hr>
-      <h1 style={{ backgroundColor: "blue", fontSize: "30px" }}>
+      <h1 style={{ backgroundColor: "lavender", fontSize: "30px" }}>
         <center>Saran R</center>
       </h1>
       <hr></hr>
