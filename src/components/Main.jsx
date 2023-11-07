f33a55e333a59815e49b2b15796b5ad486970fc2
saranyaraghava1 <727722euit166@skcet.ac.in>
2023-11-07 15:28:11 +0530

    commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,15 @@ import React from "react";
 const Main = () => {
   return (
     <div>
-      <label for="name">Email Id: &emsp;</label>
-      <input id="name" type="box" placeholder="Email ID"></input>
-      <br></br>
-      <label for="na">Password: &emsp;</label>
-      <input id="na" type="box" placeholder="Password"></input>
-      <br></br>
-      <button>Submit</button>
+      <center>
+        <label for="name">Email Id: &emsp;</label>
+        <input id="name" type="box" placeholder="Email ID"></input>
+        <br></br>
+        <label for="na">Password: &emsp;</label>
+        <input id="na" type="Password" placeholder="Password"></input>
+        <br></br>
+        <button>Submit</button>
+      </center>
     </div>
   );
 };
